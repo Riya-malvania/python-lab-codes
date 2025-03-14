5a9ea17bf863ebb3e84a06bd853749f7372e334c
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:29:14 -0700

    average.py

---
+++ average.py
@@ -0,0 +1,15 @@
+s1=int(input('enter the score of subject 1:'))
+s2=int(input('enter the  subject 2:'))
+s3=int(input('enter the score of subject 3:'))
+t=s1+s2+s3
+avg=t/3
+print('the total score is:',t)
+print('the average of three numbers is:',avg)
+
+
+output:
+enter the score of subject 1:78
+enter the score of subject 2:90
+enter the score of subject 3:95
+the total score is: 263
+the average of three numbers is: 87.66666666666667
