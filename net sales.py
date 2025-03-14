71f7297bc2f29363c4d0a64a6c6c92dbdb254229
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:23:17 -0700

    net sales.py

---
+++ net sales.py
@@ -0,0 +1,9 @@
+gross_sales=int(input('enter the sales:'))
+discount=0.1*gross_sales
+net_sales=gross_sales-discount
+print('the net sales is:',net_sales)
+
+
+output:
+enter the sales:20000
+the net sales is: 18000.0
