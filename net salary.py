8842558ba61f22a01bf5bcfa42490d950f37aba6
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:18:51 -0700

    net salary.py

---
+++ net salary.py
@@ -0,0 +1,10 @@
+gross_salary=int(input('enter the salary:'))
+allowance=0.03*gross_salary
+deduction=0.1*gross_salary
+net_salary=gross_salary+allowance-deduction
+print('the net salary is:',net_salary)
+
+
+output:
+enter the salary:50000
+the net salary is: 46500.0
