bd21b609a8d0d408ebb3b9b4dabcf665918488d8
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:46:22 -0700

    bytes to KB,MB and GB.py

---
+++ bytes to KB,MB and GB.py
@@ -0,0 +1,14 @@
+x=int(input('enter the bytes :'))
+a=x/1024
+b=x/1000000
+c=x/1000000000
+print('the bytes to kilobytes conversion is:',a)
+print('the bytes to megabytes conversion is:',b)
+print('the bytes to gigabytes conversion is:',c)
+
+
+output:
+enter the bytes :10000
+the bytes to kilobytes conversion is: 9.765625
+the bytes to megabytes conversion is: 0.01
+the bytes to gigabytes conversion is: 1e-05
