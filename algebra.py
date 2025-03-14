2a98ecff38fca5ae9120f5ab5049ee1e143fb175
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:12:22 -0700

    algebra.py

---
+++ algebra.py
@@ -0,0 +1,19 @@
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+print('the addition of two number is:',a)
+print('the subtraction of two number is:',b)
+print('the multiplication of two number is:',c)
+print('the division of two number is:',d)
+
+
+output:
+enter a number64
+enter a number12
+the addition of two number is: 76
+the subtraction of two number is: 52
+the multiplication of two number is: 768
+the division of two number is: 5.333333333333333
