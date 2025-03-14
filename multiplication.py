06561751ae9f866ded46d9179efd1a85ac912e4d
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:04:41 -0700

    multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+z=x*y
+print('the multiplication of two number is:',z)
+
+
+output:
+enter a number34
+enter a number43
+the multiplication of two number is: 1462
