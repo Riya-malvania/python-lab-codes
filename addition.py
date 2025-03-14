44449278e4683e4f92284267608e5731c11dc706
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-13 23:59:08 -0700

    addition.py

---
+++ addition.py
@@ -0,0 +1,11 @@
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+z=x+y
+print('the addition of two number is:',z)
+
+
+
+output:
+enter a number34
+enter a number45
+the addition of two number is: 79
