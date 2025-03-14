965765e885d5f4ed5f3b9ad697669df55bea845d
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 01:00:56 -0700

    interest.py

---
+++ interest.py
@@ -0,0 +1,13 @@
+P=int(input('enter the Principle amount :'))
+R=int(input('enter the Rate :'))
+N=int(input('enter the no of days :'))
+I=P*R*N/100
+print('the fahrenheit to celcius conversion is:',I)
+
+
+OUTPUT:
+enter the Principle amount :20000
+enter the Rate :5
+enter the no of days :10
+the interest is: 10000.0
+
