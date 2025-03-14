71a7c3f0554e750a260b5ec5a69486ab102b2861
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 01:59:19 -0700

    perimeter and area.py

---
+++ perimeter and area.py
@@ -0,0 +1,11 @@
+l=int(input('enter the length:'))
+p=4*l
+a=l**2
+print('the area of square is:',a)
+print('the perimeter of square is:',p)
+
+
+output:
+enter the length:6
+the area of square is: 36
+the perimeter of square is: 24
