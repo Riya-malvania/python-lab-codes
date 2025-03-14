77040f29fcb4a7b1f05b6204f1810d22627aaf00
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:01:47 -0700

    subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,11 @@
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+z=x-y
+print('the subtraction of two number is:',z)
+
+
+output:
+enter a number56
+enter a number32
+the subtraction of two number is: 24
+
