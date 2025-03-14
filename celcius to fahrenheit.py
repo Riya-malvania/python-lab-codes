9c2e87ece6b3d26e6b39c2984201341f3462096a
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:52:36 -0700

    celcius to fahrenheit.py

---
+++ celcius to fahrenheit.py
@@ -0,0 +1,9 @@
+x=int(input('enter the celcius :'))
+z=(9/5*x)+32
+print('the celcius to fahrenheit conversion is:',z)
+
+
+output:
+enter the celcius :50
+the celcius to fahrenheit conversion is: 122.0
+
