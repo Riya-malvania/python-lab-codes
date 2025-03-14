8b10cb5fb66a5a895b5ec0bb1ef95f39bbb02b50
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:03:55 -0700

    perimeter and area rectangle.py

---
+++ perimeter and area rectangle.py
@@ -0,0 +1,13 @@
+l=int(input('enter the length:'))
+b=int(input('enter the breath:'))
+p=2*(l+b)
+a=l*b
+print('the area of rectangle is:',a)
+print('the perimeter of rctangle is:',p)
+
+
+output:
+enter the length:4
+enter the breath:5
+the area of rectangle is: 20
+the perimeter of rctangle is: 18
