fdba0853c65f2f1393be49ef9980799fd9da1edb
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 00:07:20 -0700

    division.py

---
+++ division.py
@@ -0,0 +1,10 @@
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+z=x/y
+print('the division of two number is:',z)
+
+
+output:
+enter a number96
+enter a number12
+the division of two number is: 8.0
