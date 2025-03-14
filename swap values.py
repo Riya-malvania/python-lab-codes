65f3fa6f8204ca2d3b34ec172066985cef9e0800
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:38:29 -0700

    swap values.py

---
+++ swap values.py
@@ -0,0 +1,14 @@
+n1=int(input('enter the number 1:'))
+n2=int(input('enter the number 2:'))
+n1,n2=n2,n1
+print('AFTER SWAPPING')
+print('number 1 is:',n1)
+print('number 2 is:',n2)
+
+
+output:
+enter the number 1:2
+enter the number 2:4
+AFTER SWAPPING
+number 1 is: 4
+number 2 is: 2
