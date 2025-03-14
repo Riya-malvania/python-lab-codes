bfb6ac1a1b4fe078abe6919abfd47637dc7072c1
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-14 02:11:13 -0700

    area of triangle.py

---
+++ area of triangle.py
@@ -0,0 +1,9 @@
+h=int(input('enter the height:'))
+l=int(input('enter the length:'))
+a=h*l/2
+print('the area of triangle is:',a)
+
+output:
+enter the height:8
+enter the length:5
+the area of triangle is: 20.0
