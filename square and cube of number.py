65ddcc0ea138601d1aabf6b1e99908729a95ecc2
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-17 08:13:25 -0700

    square and cube of number.py

---
+++ square and cube of number.py
@@ -0,0 +1,11 @@
+x=int(input('enter the number'))
+sq=x**2
+cube=x**3
+print('the square of given number is;',sq)
+print('the cube of given number is;',cube)
+
+
+output:
+enter the number12
+the square of given number is; 144
+the cube of given number is; 1728
