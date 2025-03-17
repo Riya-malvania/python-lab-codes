8507b6963bbcc959be865dec3cb7ef4845ac1237
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-17 08:19:37 -0700

    odd or even check.py

---
+++ odd or even check.py
@@ -0,0 +1,13 @@
+x=int(input('enter the number'))
+if x%2==0:
+    print('the given number is even')
+else:
+    print('the given number is cube')
+
+
+output:
+enter the number3
+the given number is cube
+
+enter the number32
+the given number is even
