2a95678469613f43da2240c9a60a0a0693550642
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-17 08:25:49 -0700

    divisible by 7.py

---
+++ divisible by 7.py
@@ -0,0 +1,13 @@
+x=int(input('enter the number'))
+if x%7==0:
+    print('the given number is divisible by 7')
+else:
+    print('the given number is not divisible by 7')
+
+
+output:
+enter the number67
+the given number is not divisible by 7
+
+enter the number147
+the given number is divisible by 7
