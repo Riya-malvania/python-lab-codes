35f79be6b0e67392d826bd5ea185fc7e4b51d7f9
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 06:23:05 -0700

    minor and major.py

---
+++ minor and major.py
@@ -0,0 +1,14 @@
+a=int(input('enter the age of person'))
+
+if a>18:
+    print('the person is major')
+else:
+    print('the is not major')
+
+
+output:
+enter the age of person24
+the person is major
+
+enter the age of person13
+the is not major
