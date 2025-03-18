f58de2dc71cf690b2380a9858ba92b23c65c4521
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 06:17:53 -0700

    odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,13 @@
+a=int(input('enter the number'))
+print('a=',a)
+if a%2==0:
+    print('the entered number is even')
+else:
+    print('the entered number is odd')
+
+
+output:
+enter the number5
+a= 5
+the entered number is odd
+  
