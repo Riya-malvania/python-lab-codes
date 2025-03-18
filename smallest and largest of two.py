e991823a0621d49187adc8b9f5e2bac23e6a3186
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 15:46:56 +0530

    smallest and largest of two.py

---
+++ smallest and largest of two.py
@@ -0,0 +1,16 @@
+n=int(input('enter the number'))
+p=int(input('enter the number'))
+print('n=',n)
+print('p=',p)
+if n>p:
+    print('n is largest')
+else:
+    print('p is smallest')
+
+
+output:
+enter the number4
+enter the number7
+n= 4
+p= 7
+p is smallest
