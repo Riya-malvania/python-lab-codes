8b794d32990d713a5e49576d60c3021de890f1de
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 15:40:34 +0530

    greater of two num.py

---
+++ greater of two num.py
@@ -0,0 +1,16 @@
+n=int(input('enter the number'))
+p=int(input('enter the number'))
+print('n=',n)
+print('p=',p)
+if n>p:
+    print('n is greatest')
+else:
+    print('p is greatest')
+
+
+output:
+enter the number8
+enter the number9
+n= 8
+p= 9
+p is greatest
