4eb5297533be7e838c9cc22c04824fa8ddeb2746
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 06:13:49 -0700

    largest and smallest of three.py

---
+++ largest and smallest of three.py
@@ -0,0 +1,22 @@
+a=int(input('enter the number'))
+b=int(input('enter the number'))
+c=int(input('enter the number'))
+print('a=',a)
+print('b=',b)
+print('c=',c)
+if a>b and a>c:
+        print('a is greatest')
+elif b>a and b>c:
+        print('b is greatest')
+else:
+    print('c is greatest')
+
+
+output:
+enter the number2
+enter the number3
+enter the number4
+a= 2
+b= 3
+c= 4
+c is greatest
