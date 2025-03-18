df078645696b30fcf6767b2df3e35ce1b60c2938
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 06:20:35 -0700

    divisible by 10.py

---
+++ divisible by 10.py
@@ -0,0 +1,16 @@
+a=int(input('enter the number'))
+print('a=',a)
+if a%10==0:
+    print('the number is divisible by 10')
+else:
+    print('the number is not divisible by 10')
+    
+
+output:
+enter the number45
+a= 45
+the number is not divisible by 10
+
+enter the number100
+a= 100
+the number is divisible by 10
