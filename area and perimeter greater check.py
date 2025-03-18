f3c13fb24c57589661990e09dcd4ddbe74d37b75
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 07:22:02 -0700

    Create area and perimeter greater check.py

---
+++ area and perimeter greater check.py
@@ -0,0 +1,14 @@
+a=int(input('enter the length:'))
+b=int(input('enter the breath:'))
+area=a*b
+perimeter=2*(a+b)
+if area>perimeter:
+    print('the area is greater')
+else:
+    print('the area is not greater')
+
+
+output:
+enter the length:7
+enter the breath:8
+the area is greater
