f24da37a7c51cc7a449be5cefdfc0372ec9b14c4
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-18 06:55:29 -0700

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,19 @@
+a=int(input('enter the angle 1:'))
+b=int(input('enter the angle 2:'))
+c=int(input('enter the angle 3:'))
+sum=a+b+c
+if sum==180:
+    print('it is the triangle')
+else:
+    print('it is not triangle')
+
+output:
+enter the angle 1:60
+enter the angle 2:60
+enter the angle 3:60
+it is the triangle
+
+enter the angle 1:34
+enter the angle 2:57
+enter the angle 3:90
+it is not triangle
