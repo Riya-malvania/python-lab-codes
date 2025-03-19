e28268dc18ff4035c57a452a80797a13d7711acd
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-19 08:20:25 -0700

    Create equivalent.py

---
+++ equivalent.py
@@ -0,0 +1,32 @@
+x=int(input('enter the number'))
+if x==0:
+    print('0 : zero')
+if x==1:
+    print('1 : one')
+if x==2:
+    print('2 : two')
+if x==3:
+    print('3 : three')
+if x==4:
+    print('4 : four')
+if x==5:
+    print('5 : five')
+if x==6:
+    print('6 : six')
+if x==7:
+    print('7 : seven')
+if x==8:
+    print('8 : eight')
+if x==9:
+    print('9 : nine')
+if x==10:
+    print('10 : ten')
+if x==11:
+  print('11 : eleven')
+if x==12:
+    print('12 : twelve')
+
+
+ouutput:
+enter the number4
+4 : four
