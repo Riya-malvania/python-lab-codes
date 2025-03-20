f80b7f4c8f4d2a3c016f71af110693b011cea4ba
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-20 09:40:40 -0700

    Create uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,14 @@
+l1=['riya','shikha','jeet','vansh','aarav']
+print('before uppercase conversion')
+print(l1)
+l2=[]
+for i in l1:
+    l2.append(i.upper())
+print('after uppercase conversion')
+print(l2)
+
+output:
+before uppercase conversion
+['riya', 'shikha', 'jeet', 'vansh', 'aarav']
+after uppercase conversion
+['RIYA', 'SHIKHA', 'JEET', 'VANSH', 'AARAV']
