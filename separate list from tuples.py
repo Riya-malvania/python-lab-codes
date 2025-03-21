347352d07fd31ed7ce5663e85c76cab0db70b5c1
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-03-21 08:05:09 -0700

    Create separate list from tuples.py

---
+++ separate list from tuples.py
@@ -0,0 +1,16 @@
+l=[(1,2,3,4,5),('riya','shikha','aarav','vansh','jeet'),(18,24,11,16,23)]
+l1=[]
+l2=[]
+l3=[]
+l1.append(l[0])
+l2.append(l[1])
+l3.append(l[2])
+print(l1)
+print(l2)
+print(l3)
+
+
+output:
+[(1, 2, 3, 4, 5)]
+[('riya', 'shikha', 'aarav', 'vansh', 'jeet')]
+[(18, 24, 11, 16, 23)]
