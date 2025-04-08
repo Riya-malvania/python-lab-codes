34bb468aa2d9fa5b785684510f7396b1ec1bcdbb
Riya-malvania <24bch143@spt.pdpu.ac.in>
2025-04-08 07:05:48 -0700

    Create star pattern.py

---
+++ star pattern.py
@@ -0,0 +1,12 @@
+for i in range(3,0,-1):
+    for j in range(i):
+        print('*',end='')
+    print()
+
+
+
+output:
+=================== RESTART: C:/Users/user/Desktop/1.py ===================
+***
+**
+*
